2c5f49cc66a5e75fdedd31aab0480852f6dc45bd
dran5447 <danielle.r.neuberger@gmail.com>
2019-01-21 12:31:00 -0800

    add person cache

--- lib/shared/datastore.dart
+++ lib/shared/datastore.dart
@@ -7,6 +7,11 @@ import '../model/event.dart';
 import '../model/idea.dart';
 
 class DataStore{
+  Map<String, Person> peopleCache = new Map<String, Person>();
+  //Map<String, Idea> ideaCache;
+  //Map<String, Event> eventCache;
+
+
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
     return directory.path;
@@ -39,11 +44,12 @@ class DataStore{
      //Encode person to JSON format
     String json = jsonEncode(person);
 
+    //Add to cached person list
+    peopleCache[person.id] = person;
+
     //Get contents of file
     try {
       var contents = file.readAsStringSync();
-
-      //TODO later change filemode to append to save resources/time
 
       //if first entry, add [ ] and content, else add data
       if(contents == ""){
@@ -73,6 +79,11 @@ class DataStore{
 
       var parsedJson = json.decode(contents); 
       var result = parsedJson.map<Person>((item) => (new Person.fromJson(item))).toList();
+
+      //Update cached list
+      for(Person p in result){
+        peopleCache[p.id] = p;
+      }
 
       return result;
     } catch (e) {
